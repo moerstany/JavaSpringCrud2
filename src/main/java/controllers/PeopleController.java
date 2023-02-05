7b1e3a449852dba70037466b86c9ec24e7b69341
moerstany <moerstany@gmail.com>
2023-02-05 23:33:44 +0700

    вывод на страницы

--- src/main/java/controllers/PeopleController.java
+++ src/main/java/controllers/PeopleController.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import dao.BuyersDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,18 +9,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/people")
+@RequestMapping("/buyers")
 public class PeopleController {
+    private final BuyersDao buyersDao;
+    @Autowired
+    public PeopleController(BuyersDao buyersDao) {
+        this.buyersDao = buyersDao;
+    }
 
     @GetMapping
     public String index(Model model){
         //получаем всех людей из базы
-        return null;
+        model.addAttribute("webapp", buyersDao.index());
+        return "webapp/index";
     }
 
     @GetMapping("/{id}")
     public String show(@PathVariable("id")int id, Model model){
         //получим 1 человека по id из базы
-        return null;
+        model.addAttribute("webapp", buyersDao.show(id));
+        return "webapp/show";
     }
 }
--- src/main/java/dao/BuyersDao.java
+++ src/main/java/dao/BuyersDao.java
@@ -1,4 +1,24 @@
 package dao;
 
+import models.Buyers;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+
+import java.util.ArrayList;
+import java.util.List;
+@Component
 public class BuyersDao {
+    private List <Buyers> buyers;
+    {
+        buyers = new ArrayList<>();
+        buyers.add(new Buyers(1,"Никита","Орлов","23@ya.ru","Владимир","Россия"));
+        buyers.add(new Buyers(2,"Юлия","Мельникова","25@ya.ru","Сочи","Россия"));
+    }
+
+    public List<Buyers> index() {
+        return buyers;
+    }
+    public Buyers show (int id){
+        return buyers.stream().filter(buyers -> buyers.getId()==id).findAny().orElse(null);
+    }
 }
